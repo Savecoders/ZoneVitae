0b981a41990a79ed8d67ca64c1d382abc42becc9
Savecoders <pablopincay2003@gmail.com>
2025-05-13 23:59:54 -0500

    feat: refactor app configuration and define application routes

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,9 +1,26 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import { DEFAULT_UI_CONFIG, UI_CONFIG } from './config/ui.config';
+import { ThemeService } from './services/theme.service';
+import { ColorUtilService } from './services/color-util.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    { provide: UI_CONFIG, useValue: DEFAULT_UI_CONFIG },
+    ThemeService,
+    ColorUtilService,
+  ],
 };
