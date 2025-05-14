3c064a6de4ec97829263bef20a3580f970065060
Savecoders <pablopincay2003@gmail.com>
2025-05-14 17:30:35 -0500

    feat: add Auditoria component with layout

---
+++ client/src/app/components/auditoria/auditoria.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { LayoutComponent } from "../shared/layout/layout.component";
+
+@Component({
+  selector: 'app-auditoria',
+  imports: [LayoutComponent],
+  templateUrl: './auditoria.component.html',
+  styleUrl: './auditoria.component.css'
+})
+export class AuditoriaComponent {
+
+}
