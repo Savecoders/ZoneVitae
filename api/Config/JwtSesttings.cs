8f1bfa058baadb4ba254cba1271366fae8d67c15
Savecoders <pablopincay2003@gmail.com>
2025-07-01 21:27:47 -0500

    feat: (api) Add JWT settings properties and expiration config

--- api/Config/JwtSesttings.cs
+++ api/Config/JwtSesttings.cs
@@ -2,5 +2,8 @@ namespace api.Config;
 
 public class JwtSesttings
 {
-    
+    public required string Key { get; set; }
+    public required string Issuer { get; set; }
+    public required string Audience { get; set; }
+    public required int ExpirationInMinutes { get; set; } = 4320;
 }
