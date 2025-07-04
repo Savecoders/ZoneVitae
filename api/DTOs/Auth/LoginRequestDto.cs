734a43823049f11027387fd913ed3e1d55aeed52
Savecoders <pablopincay2003@gmail.com>
2025-07-03 23:46:13 -0500

    chore: validations and notations in dtos auth

--- api/DTOs/Auth/LoginRequestDto.cs
+++ api/DTOs/Auth/LoginRequestDto.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace api.DTOs.Auth
 {
     public class LoginRequestDto
     {
+        [Required(ErrorMessage = "El email es requerido")]
+        [EmailAddress(ErrorMessage = "El formato del email no es válido")]
         public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "La contraseña es requerida")]
+        [MinLength(8, ErrorMessage = "La contraseña debe tener al menos 8 caracteres")]
         public string Password { get; set; } = null!;
     }
 }
